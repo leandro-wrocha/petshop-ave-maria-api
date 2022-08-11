01dacf5a17717bca5533835037f98a9cbd9be871
Leandro Rocha <leandro.wrocha98@gmail.com>
2022-08-11 15:54:25 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import 'express-async-errors';
 
 import express, { NextFunction, Request, Response } from "express";
+import cors from 'cors';
 
 import { AppError } from '../../errors/AppError';
 import { routes } from "./routes";
@@ -12,6 +13,7 @@ import "../../container";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((error: AppError, request: Request, response: Response, next: NextFunction) => {
